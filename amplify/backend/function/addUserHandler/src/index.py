adf254142952ea748bcdebcc1c1bee0745bc66b3
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-07-06 23:59:55 +0200

    :construction: WIP add function

--- amplify/backend/function/addUserHandler/src/index.py
+++ amplify/backend/function/addUserHandler/src/index.py
@@ -5,14 +5,24 @@ import uuid
 
 def handler(event, context):
     table_name = os.environ.get("STORAGE_USERTABLE_NAME", "UserTable")
-    dynamodb = boto3.resource('dynamodb', region_name = "eu-west-1")
+    dynamodb = boto3.resource('dynamodb', region_name="eu-west-1")
     table = dynamodb.Table(table_name)
+
+    # Sécurité : utiliser get() pour éviter KeyError
+    name = event.get('name')
+    email = event.get('email')
+
+    if not name or not email:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Missing name or email')
+        }
 
     table.put_item(
         Item={
             'user_id': str(uuid.uuid4()),
-            'name': event['name'],
-            'email': event['email']
+            'name': name,
+            'email': email
         }
     )
 
